2f3801de998d93559fac4cbf24ad377669cd19f2
qroku <eabooobe@gmail.com>
2018-06-05 21:44:05 +0900

    2-1 2-4

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello',function(){
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これはサンプルページです。</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello',function() use ($html){
+    return $html;
 });

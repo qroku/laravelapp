7700849a826cd27fc9f017f0f01944e3f8d338bb
qroku <eabooobe@gmail.com>
2018-06-07 20:14:23 +0900

    2-2 2-12

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,25 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HelloController extends Controller
-{
-    
-    public function index() {
-        return <<<EOF
-<html>
-<head>
-<title>Hello/index</title>
+global $head, $style ,$body ,$end;
+$head = '<html><head>';
+$style = <<<EOF
 <style>
 body { font-size:16pt; color:#999; }
 h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
 </style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>これはHelloコントローラーのindexアクションです。</p>
-</body>
-</html>
 EOF;
+$body = '</head><body>';
+$end = '</body></html>';
 
+function tag($tag, $txt) {
+    return "<{$tag}>" .$txt ."</{$tag}>";
+}
+
+class HelloController extends Controller
+{
+    
+    public function index() {
+        global $head, $style ,$body ,$end;
+
+        $html = $head .tag('title','Hello/index') .$style .$body
+                .tag('h1','Index') .tag('p','this is Index page.')
+                .'<a href="/hello/other">go to Other page</a>'
+                .$end;
+        return $html;
+    }
+
+    public function other() {
+        global $head ,$style ,$body ,$end;
+
+        $html = $head .tag('title','Hello/other') .$style .$body
+                . tag('h1','Other') .tag('p','this is Other page.')
+                . $end;
+
+        return $html;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::get('hello' ,'HelloController@index');
+Route::get('hello/other' ,'HelloController@other');

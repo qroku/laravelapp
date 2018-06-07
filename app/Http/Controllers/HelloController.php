e46545a6b3a95fda2da164f53943ceb36f74fc86
qroku <eabooobe@gmail.com>
2018-06-07 20:37:08 +0900

    2-2 2-16

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,42 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-global $head, $style ,$body ,$end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-body { font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt) {
-    return "<{$tag}>" .$txt ."</{$tag}>";
-}
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    
-    public function index() {
-        global $head, $style ,$body ,$end;
 
-        $html = $head .tag('title','Hello/index') .$style .$body
-                .tag('h1','Index') .tag('p','this is Index page.')
-                .'<a href="/hello/other">go to Other page</a>'
-                .$end;
-        return $html;
-    }
+    public function index(Request $request ,Response $response) {
+        $html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size:16pt; color:#999; }
+h1 { font-size:120pt; text-align:right; color:#fafafa;
+    margin:-50px 0px 0px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Response</h3>
+    <pre>{$response}</pre>
+</body>
+</html>
+EOF;
 
-    public function other() {
-        global $head ,$style ,$body ,$end;
-
-        $html = $head .tag('title','Hello/other') .$style .$body
-                . tag('h1','Other') .tag('p','this is Other page.')
-                . $end;
-
-        return $html;
+        $response->setContent($html);
+        return $response;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,3 @@ Route::get('/', function () {
 });
 
 Route::get('hello' ,'HelloController@index');
-Route::get('hello/other' ,'HelloController@other');

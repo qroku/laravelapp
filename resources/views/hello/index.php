180e4d1f3942af7e021602de64774e4a04d121de
qroku <eabooobe@gmail.com>
2018-06-08 20:05:08 +0900

    3-1 3-2

---
+++ resources/views/hello/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#f6f6f6;
+    margin:-50px 0px 0px 0px; }
+</style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p>This is sample page with php-template.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello' ,'HelloController@index');
+Route::get('hello',function() {
+    return view('hello.index');
+});

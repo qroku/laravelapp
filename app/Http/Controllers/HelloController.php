116b0914bf631d6b0dcf3c5174b71c1d80b6a145
qroku <eabooobe@gmail.com>
2018-07-03 20:03:59 +0900

    ~3-20

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,16 +10,11 @@ class HelloController extends Controller
 
     public function index()
     {
-        $data = [
-            'msg' => 'これはコントローラから渡されたメッセージです。'
-        ];
-        return view('hello.index',$data);
+        return view('hello.index');
     }
 
     public function post(Request $request)
     {
-        $msg = $request->msg;
-        $data = [ 'msg' => 'こんにちは、' .$msg .'さん'];
-        return view('hello.index',$data);
+        return view('hello.index',['msg'=> $request->msg]);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,7 +9,11 @@ h1 { font-size:50pt; text-align:right; color:#f6f6f6;
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>{{$msg}}</p>
+    @isset ($msg)
+    <p>こんにちは、{{$msg}}さん。</p>
+    @else
+    <p>何か書いてください。</p>
+    @endisset
     <form method="POST" action="/hello">
         {{ csrf_field() }}
         <input type="text" name="msg">

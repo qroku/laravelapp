3751f435fa872cdbf497008c40ca676f621fbe00
qroku <eabooobe@gmail.com>
2018-10-02 22:41:29 +0900

    ~4-22

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
@@ -13,15 +14,8 @@ class HelloController extends Controller
         return view('hello.index',['msg'=>'フォームを入力: ']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-
-        $this->validate($request,$validate_rule);
-        return view('hello.index',['msg'=>'正しく入力されました！']);
+        return view('hello.index',['msg' => '正しく入力されまｈした！']);
     }
 }
---
+++ app/Http/Requests/HelloRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HelloRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($this->path() == 'hello')
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.between' => '年齢は0~150の間で入力してください。',
+        ];
+    }
+}
